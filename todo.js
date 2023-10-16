a6cced5c7c3b3ee55d386abe10a6b0466d412758
unknown <wanjikuclare82@gmail.com>
2023-10-16 16:13:36 +0300

    created remove function

--- todo.js
+++ todo.js
@@ -1,88 +1,54 @@
+const input = document.getElementById("new_task_input");
+const btn = document.getElementById("btn");
+const task_container = document.getElementById("task_container");
 
-   const input = document.getElementById('new_task_input');
-   const btn = document.getElementById('btn');
-   const list = document.getElementById('list');
- 
-   btn.addEventListener('click', handleclick);
-      preventDefault();
-      stopPropagation();
-     
+btn.addEventListener("click", handleclick);
+function handleclick(event) {
+  event.preventDefault();
+  event.stopPropagation();
 
-  let new_title= input.value;
-  if(new_title.trim () !=="") {
-    let new_task= createTask(new_title);
+  let new_title = input.value;
+  if (new_title.trim() !== "") {
+    let new_task = createTask(new_title);
     mountTask2Dom(new_task);
-  }
-  else{
+  } else {
     alert("task cannot be empty");
   }
-  input.value=""
+  input.value = "";
+}
 
-  function createTask(title){
+function createTask(to_do) {
+  const new_task = document.createElement("div");
+  new_task.classList.add("task");
 
-    const new_task= document.createElement('div');
-    new_task.classList.add('task');
-    
-     const span = document.createElement('span');
-     span.innerText= title;
+  const span = document.createElement("span");
+  span.innerText = to_do;
 
+  const delete_button = document.createElement("button");
+  delete_button.innerText = "delete";
 
-    const delete_button = document.createElement('button');
-     delete_button.innerText= "delete";
+  // append task
 
-     // append task
+  delete_button.addEventListener("click", () => {
+    new_task.remove();
+  });
 
-    new_task.append(span,delete_button);
-    return new_task; 
-  }
+  new_task.append(span, delete_button);
+  return new_task;
+}
 
+function mountTask2Dom(task_node) {
+  task_container.append(task_node);
+}
 
-  function mountTask2Dom(task_node){
-    task_container.append(task_node);
-  }
+// deleting task
 
-  // deleting task
+//task_container.addEventListener('click', handleDelete);
+//function handleDelete(event){
+//  const targetElement = event.target;
+// if(targetElement.innerText==="delete"){
+  //     task_container.removeChild(targetElement.parentNode)
+  
+//} else if(targetElement.localName==="span"){}
 
-  task_container.addEventListener('click', handleDelete);
-  function handleDelete(event){
-      const targetElement = event.target;
-
-
-  }
-
-     
-    /* if (trimmed_text !== "") {
-       const listItem = document.createElement('li');
-       listItem.textContent = trimmed_text;
-       listItem.className = 'todo-item';
-       listItem.dataset.completed = 'false';*/
-
-       const deleteBtn = document.createElement('button');
-       deleteBtn.textContent = 'Delete';
-       deleteBtn.className = 'delete-button';
-       deleteBtn.addEventListener('click', function() {
-         listItem.remove(); 
-       });
-
-       listItem.addEventListener('click', function() {
-         if (listItem.dataset.completed === 'false') {
-           listItem.dataset.completed = 'true';
-           listItem.style.textDecoration = 'line-through'; 
-         } else {
-           listItem.dataset.completed = 'false';
-           listItem.style.textDecoration = 'none'; 
-         }
-       });
-
-       
-      
-       listItem.appendChild(deleteBtn);
- 
-       
-       list.appendChild(listItem);
- 
-      
-       new_task_input.value = '';
-     }
-   });
- });
+//}
